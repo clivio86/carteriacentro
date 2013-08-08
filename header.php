4e352cf8d60f73445799fb4efc074f5a3fc2e2f0
Clivio Fenili <clivio@clivio.me>
2013-08-08 03:41:47 +0200

    seconda lezione

---
+++ header.php
@@ -0,0 +1,42 @@
+<!-- Inizio Header -->
+ 
+<!doctype html>
+ 
+<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
+<!--[if lt IE 7]> <html lang="en"> <![endif]-->
+<!--[if IE 7]>    <html lang="en"> <![endif]-->
+<!--[if IE 8]>    <html lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+ 
+    <head>
+ 
+        <meta charset="utf-8">
+ 
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+ 
+        <title></title>
+ 
+        <meta name="description" content="">
+ 
+        <meta name="author" content="">
+ 
+        <meta name="viewport" content="width=device-width,initial-scale=1">
+ 
+ <!-- Inserito il programma less per il css-->
+	<link rel="stylesheet/less" type="text/css" href="<?php bloginfo( 'template_url'); ?>/less/style.less">
+ 	<script src="<?php bloginfo( 'template_url'); ?>/js/libs/less.js" type="text/javascript"></script>
+
+			 
+        <script src="js/libs/modernizr.min.js"></script>
+ 
+    </head>
+ 
+    <body>
+ 
+        <div id="container">
+ 
+            <header>
+ 
+            </header>
+ 
+<!-- Fine Header -->
