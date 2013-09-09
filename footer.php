962b24e402f0039d5d69fa97c98daa28cc99c206
clivio86 <clivio@clivio.me>
2013-09-09 14:00:56 +0200

    Aggiunt Credits e Cambio colori font

--- footer.php
+++ footer.php
@@ -6,6 +6,8 @@
 			
 		 	<div class=piepagina>La Carteria del Centro <br> Via A. Serpieri, 30 - 47921 Rimini RN <br> P.IVA: 02064210400 <br>
 		 		<a href="mailto:info@lacarteriadelcentro.it" /> info@lacarteriadelcentro.it</a></div>
+		 		<div style="clear:both;"></div>
+		 		<div class="credits" />  Web Designe: Sabrina Cenni - Webmaster: Clivio Fenili - PH: Veronica Bronzetti</div>
 		</footer>
 	
 	</div> <!-- /#container -->
--- functions.php
+++ functions.php
@@ -117,6 +117,21 @@
 	
 	
 	
+/*
+ * Togliere barra admin dal template front office.
+ */
+	
+	function my_function_admin_bar(){
+	    return false;
+	} 
+	add_filter( 'show_admin_bar' , 'my_function_admin_bar');
+
+	
+	
+	
+	
+	
+	
 	
 	
 	
